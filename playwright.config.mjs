aaac447251c7ee94a7bc9a8a86c1c1d8349e8b88
ericva01 <ericva014@gmail.com>
2026-09-23 11:49:14 +0700

    first commit

--- playwright.config.mjs
+++ playwright.config.mjs
@@ -5,15 +5,15 @@ export default defineConfig({
   timeout: 30000,
   workers: 1,
   use: {
-    baseURL: "http://localhost:3000",
+    baseURL: "http://localhost:3005",
     channel: "chrome",
     headless: true,
     trace: "retain-on-failure",
   },
   webServer: {
-    command: "npm run start",
-    port: 3000,
-    reuseExistingServer: true,
-    timeout: 15000,
+    command: "npx next start -p 3005",
+    port: 3005,
+    reuseExistingServer: false,
+    timeout: 20000,
   },
 });
